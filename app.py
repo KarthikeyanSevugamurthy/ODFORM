d821485d2f2c61d3a0bc34e31e0f57af60d2b784
KarthikeyanSevugamurthy <139134795+KarthikeyanSevugamurthy@users.noreply.github.com>
2023-12-26 20:21:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ def index():
 
     return render_template('index.html', download_links=download_links)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+ #   app.run(debug=True)
